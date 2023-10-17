324da5507744e8e82ed33647729f10ebeba69e98
OlyaTolmacheva <139631253+OlyaTolmacheva@users.noreply.github.com>
2023-10-17 13:51:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string [] array = {"1", "sun", "54067", "Non-fiction", "-8", "mix", "moon", ":)"};
+
+for (int i = 0; i<array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
